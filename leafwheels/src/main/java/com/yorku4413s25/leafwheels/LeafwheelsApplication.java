12b7d37a1ba8c1455b2b81a0a47dc73bfacec7a0
Mason Wang <mason123wp@gmail.com>
2025-06-10 03:06:35 -0400

    Initialize Spring Boot project with Maven wrapper and basic configuration

---
+++ leafwheels/src/main/java/com/yorku4413s25/leafwheels/LeafwheelsApplication.java
@@ -0,0 +1,13 @@
+package com.yorku4413s25.leafwheels;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeafwheelsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LeafwheelsApplication.class, args);
+    }
+
+}
