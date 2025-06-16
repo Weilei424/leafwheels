21848bf5614fa7cec30065a71245dffe44105048
Mason Wang <mason123wp@gmail.com>
2025-06-16 02:53:02 -0400

    Extend VehicleRepository to support paging and sorting for Vehicle entities

--- leafwheels/src/main/java/com/yorku4413s25/leafwheels/repositories/VehicleRepository.java
+++ leafwheels/src/main/java/com/yorku4413s25/leafwheels/repositories/VehicleRepository.java
@@ -1,4 +1,9 @@
 package com.yorku4413s25.leafwheels.repositories;
 
-public interface VehicleRepository {
+import com.yorku4413s25.leafwheels.domain.Vehicle;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+import java.util.UUID;
+
+public interface VehicleRepository extends PagingAndSortingRepository<Vehicle, UUID> {
 }
