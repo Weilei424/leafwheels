a5af0d73b91dd9d725e8d3556d918a988b78d062
Mason Wang <mason123wp@gmail.com>
2025-06-16 02:48:53 -0400

    Add VehicleController for vehicle API endpoints

---
+++ leafwheels/src/main/java/com/yorku4413s25/leafwheels/web/controllers/VehicleController.java
@@ -0,0 +1,12 @@
+package com.yorku4413s25.leafwheels.web.controllers;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/api/v1/vehicles")
+@AllArgsConstructor
+@Controller
+public class VehicleController {
+
+}
