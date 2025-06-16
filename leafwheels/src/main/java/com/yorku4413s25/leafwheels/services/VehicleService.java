d14fe88c28b96a4117d683384a7e9ac65ca4bb59
Mason Wang <mason123wp@gmail.com>
2025-06-16 02:49:26 -0400

    Add VehicleService interface and implementation for vehicle business logic

---
+++ leafwheels/src/main/java/com/yorku4413s25/leafwheels/services/VehicleService.java
@@ -0,0 +1,4 @@
+package com.yorku4413s25.leafwheels.services;
+
+public interface VehicleService {
+}
---
+++ leafwheels/src/main/java/com/yorku4413s25/leafwheels/services/VehicleServiceImpl.java
@@ -0,0 +1,9 @@
+package com.yorku4413s25.leafwheels.services;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@AllArgsConstructor
+@Service
+public class VehicleServiceImpl implements VehicleService {
+}
