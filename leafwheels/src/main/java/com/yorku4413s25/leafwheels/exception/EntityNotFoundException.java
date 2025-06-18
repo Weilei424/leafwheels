4436766383f444484d685bb7c81134fec51dcdad
Mason Wang <mason123wp@gmail.com>
2025-06-18 16:28:27 -0400

    Update EntityNotFoundException to support UUID as identifier

--- leafwheels/src/main/java/com/yorku4413s25/leafwheels/exception/EntityNotFoundException.java
+++ leafwheels/src/main/java/com/yorku4413s25/leafwheels/exception/EntityNotFoundException.java
@@ -1,7 +1,9 @@
 package com.yorku4413s25.leafwheels.exception;
 
+import java.util.UUID;
+
 public class EntityNotFoundException extends RuntimeException {
-    public EntityNotFoundException(Long id, Class<?> entity) {
-        super("The " + entity.getSimpleName().toLowerCase() + " with id '" + id + "' does not exist");
+    public EntityNotFoundException(UUID id, Class<?> entity) {
+        super("The " + entity.getSimpleName().toLowerCase() + " with id '" + id.toString() + "' does not exist");
     }
 }
