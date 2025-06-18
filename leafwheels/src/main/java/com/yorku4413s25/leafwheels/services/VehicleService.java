d0cc31dc5c29654be5b2d6909d5f9d0b217b5d67
Mason Wang <mason123wp@gmail.com>
2025-06-18 16:31:47 -0400

    Implement VehicleService methods for CRUD operations and add vehicle retrieval logic

--- leafwheels/src/main/java/com/yorku4413s25/leafwheels/services/VehicleService.java
+++ leafwheels/src/main/java/com/yorku4413s25/leafwheels/services/VehicleService.java
@@ -1,4 +1,18 @@
 package com.yorku4413s25.leafwheels.services;
 
+import com.yorku4413s25.leafwheels.web.models.VehicleDto;
+
+import java.util.List;
+import java.util.UUID;
+
 public interface VehicleService {
+    VehicleDto getById(UUID vehicleId);
+
+    VehicleDto create(VehicleDto vehicleDto);
+
+    void update(UUID vehicleId, VehicleDto vehicleDto);
+
+    void delete(UUID vehicleId);
+
+    List<VehicleDto> getAllVehicles();
 }
