3e03a9810d620be647abf7b8cd20baecebb4a665
Mason Wang <mason123wp@gmail.com>
2025-06-19 02:35:25 -0400

    Add @Repository annotation to VehicleRepository and extend CrudRepository

--- leafwheels/src/main/java/com/yorku4413s25/leafwheels/repositories/VehicleRepository.java
+++ leafwheels/src/main/java/com/yorku4413s25/leafwheels/repositories/VehicleRepository.java
@@ -1,9 +1,12 @@
 package com.yorku4413s25.leafwheels.repositories;
 
 import com.yorku4413s25.leafwheels.domain.Vehicle;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
-public interface VehicleRepository extends PagingAndSortingRepository<Vehicle, UUID> {
+@Repository
+public interface VehicleRepository extends PagingAndSortingRepository<Vehicle, UUID>, CrudRepository<Vehicle, UUID> {
 }
