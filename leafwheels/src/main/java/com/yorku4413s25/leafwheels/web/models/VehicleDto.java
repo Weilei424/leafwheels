d3e71331139415f7227e45d309eb34c59c3b5a4d
Mason Wang <mason123wp@gmail.com>
2025-06-19 03:01:29 -0400

    Refactor VehicleDto to use Lombok's @Value and include new fields for year, make, bodyType, and other vehicle attributes

--- leafwheels/src/main/java/com/yorku4413s25/leafwheels/web/models/VehicleDto.java
+++ leafwheels/src/main/java/com/yorku4413s25/leafwheels/web/models/VehicleDto.java
@@ -1,19 +1,38 @@
 package com.yorku4413s25.leafwheels.web.models;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import com.yorku4413s25.leafwheels.constants.BodyType;
+import com.yorku4413s25.leafwheels.constants.Condition;
+import com.yorku4413s25.leafwheels.constants.Make;
+import com.yorku4413s25.leafwheels.constants.VehicleStatus;
+import lombok.Value;
 
+import java.io.Serializable;
+import java.math.BigDecimal;
 import java.util.UUID;
 
 /**
  * DTO for {@link com.yorku4413s25.leafwheels.domain.Vehicle}
  */
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
-@Builder
-public class VehicleDto  {
+@Value
+public class VehicleDto implements Serializable {
     UUID id;
+    int year;
+    Make make;
+    String model;
+    BodyType bodyType;
+    String exteriorColor;
+    int doors;
+    int seats;
+    int mileage;
+    int batteryRange;
+    String trim;
+    BigDecimal price;
+    boolean onDeal;
+    String vin;
+    BigDecimal discountPercent;
+    Condition condition;
+    String description;
+    VehicleStatus status;
+
+
 }
