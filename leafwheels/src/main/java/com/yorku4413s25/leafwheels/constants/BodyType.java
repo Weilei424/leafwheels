595e7a80f5eee36c1efdfbb42a05adb2f9660761
Mason Wang <mason123wp@gmail.com>
2025-06-19 02:49:42 -0400

    Add enums for BodyType, Condition, Make, and VehicleStatus

---
+++ leafwheels/src/main/java/com/yorku4413s25/leafwheels/constants/BodyType.java
@@ -0,0 +1,12 @@
+package com.yorku4413s25.leafwheels.constants;
+
+public enum BodyType {
+    SEDAN,
+    SUV,
+    HATCHBACK,
+    COUPE,
+    CONVERTIBLE,
+    WAGON,
+    MINIVAN,
+    TRUCK
+}
---
+++ leafwheels/src/main/java/com/yorku4413s25/leafwheels/constants/Make.java
@@ -0,0 +1,21 @@
+package com.yorku4413s25.leafwheels.constants;
+
+public enum Make {
+    TESLA,
+    NISSAN,
+    CHEVROLET,
+    FORD,
+    AUDI,
+    BMW,
+    HYUNDAI,
+    KIA,
+    VOLKSWAGEN,
+    PORSCHE,
+    JAGUAR,
+    RIVIAN,
+    LUCID,
+    MERCEDES_BENZ,
+    VOLVO,
+    POLESTAR,
+    OTHER
+}
