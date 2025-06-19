1a5b6c86a3c61c8bdea475b45bfcc2e90276f1c0
Mason Wang <mason123wp@gmail.com>
2025-06-19 03:01:19 -0400

    Add method to update existing Vehicle entity from VehicleDto in VehicleMapper

--- leafwheels/src/main/java/com/yorku4413s25/leafwheels/web/mappers/VehicleMapper.java
+++ leafwheels/src/main/java/com/yorku4413s25/leafwheels/web/mappers/VehicleMapper.java
@@ -3,9 +3,11 @@ package com.yorku4413s25.leafwheels.web.mappers;
 import com.yorku4413s25.leafwheels.domain.Vehicle;
 import com.yorku4413s25.leafwheels.web.models.VehicleDto;
 import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
 
 @Mapper(uses = DateMapper.class)
 public interface VehicleMapper {
     VehicleDto vehicleToVehicleDto(Vehicle vehicle);
     Vehicle vehicleDtoToVehicle(VehicleDto vehicleDto);
+    void vehicleDtoToVehicleUpdate(VehicleDto vehicleDto, @MappingTarget Vehicle existing);
 }
