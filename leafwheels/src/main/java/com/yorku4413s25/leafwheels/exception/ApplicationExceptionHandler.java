735c5d68a70773bb6ec8ff6a82df10ca5c7714dc
Mason Wang <mason123wp@gmail.com>
2025-06-19 10:05:45 -0400

    Add ApplicationExceptionHandler for centralized exception handling

---
+++ leafwheels/src/main/java/com/yorku4413s25/leafwheels/exception/ApplicationExceptionHandler.java
@@ -0,0 +1,17 @@
+package com.yorku4413s25.leafwheels.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.Arrays;
+
+@ControllerAdvice
+public class ApplicationExceptionHandler extends ResponseEntityExceptionHandler {
+
+    public ResponseEntity<Object> handleResourceNotFoundException(RuntimeException ex) {
+        ErrorResponse error = new ErrorResponse(Arrays.asList(ex.getMessage()));
+        return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
+    }
+}
