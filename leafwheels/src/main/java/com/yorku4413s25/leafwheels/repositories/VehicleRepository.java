92faa674a5872e33991c5ba3f188f4b6ca58686a
Your Name <tarang5757@gmail.com>
2025-06-21 13:37:31 -0400

    "This pull request is to resolve the build process, no code line changes."

--- leafwheels/src/main/java/com/yorku4413s25/leafwheels/repositories/VehicleRepository.java
+++ leafwheels/src/main/java/com/yorku4413s25/leafwheels/repositories/VehicleRepository.java
@@ -8,9 +8,5 @@ import org.springframework.stereotype.Repository;
 import java.util.UUID;
 
 @Repository
-// PagingAndSortingRepository already includes everything CRUDRepository offers.
-// public interface VehicleRepository extends PagingAndSortingRepository<Vehicle, UUID>, CrudRepository<Vehicle, UUID>
-// -> public interface VehicleRepository extends PagingAndSortingRepository<Vehicle, UUID>
-public interface VehicleRepository extends PagingAndSortingRepository<Vehicle, UUID> {
-
+public interface VehicleRepository extends PagingAndSortingRepository<Vehicle, UUID>, CrudRepository<Vehicle, UUID> {
 }
