1ec7bc67500bcb92679f8396ab2c7ce113e71b6a
Mason Wang <mason123wp@gmail.com>
2025-06-21 01:52:00 -0400

    Refactor VehicleController to use @RestController for improved RESTful API handling

--- leafwheels/src/main/java/com/yorku4413s25/leafwheels/web/controllers/VehicleController.java
+++ leafwheels/src/main/java/com/yorku4413s25/leafwheels/web/controllers/VehicleController.java
@@ -1,12 +1,11 @@
 package com.yorku4413s25.leafwheels.web.controllers;
 
 import lombok.AllArgsConstructor;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RequestMapping("/api/v1/vehicles")
 @AllArgsConstructor
-@Controller
+@RestController
 public class VehicleController {
-
 }
