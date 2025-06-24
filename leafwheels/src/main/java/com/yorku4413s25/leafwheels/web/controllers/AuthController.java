a89c55211ea600f3f3796ab1fa05e42833a82e48
Mason Wang <mason123wp@gmail.com>
2025-06-24 02:43:11 -0400

    Update AuthController to use versioned API endpoint "/api/v1/auth"

--- leafwheels/src/main/java/com/yorku4413s25/leafwheels/web/controllers/AuthController.java
+++ leafwheels/src/main/java/com/yorku4413s25/leafwheels/web/controllers/AuthController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.*;
 
 
 @RestController
-@RequestMapping("/auth")
+@RequestMapping("/api/v1/auth")
 public class AuthController {
 
     private AuthService authService;
