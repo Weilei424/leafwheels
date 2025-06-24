de34333fd0f2a6beff4f7d5596553f306a96b5dd
Mason Wang <mason123wp@gmail.com>
2025-06-24 02:44:18 -0400

    Refactor VehicleRepository to extend JpaRepository for improved data access

--- leafwheels/src/main/java/com/yorku4413s25/leafwheels/repositories/VehicleRepository.java
+++ leafwheels/src/main/java/com/yorku4413s25/leafwheels/repositories/VehicleRepository.java
@@ -1,12 +1,12 @@
 package com.yorku4413s25.leafwheels.repositories;
 
 import com.yorku4413s25.leafwheels.domain.Vehicle;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
 @Repository
-public interface VehicleRepository extends PagingAndSortingRepository<Vehicle, UUID>, CrudRepository<Vehicle, UUID> {
+public interface VehicleRepository extends PagingAndSortingRepository<Vehicle, UUID>, JpaRepository<Vehicle, UUID> {
 }
