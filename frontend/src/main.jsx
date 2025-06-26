41ec94d0e417fb1d95eb5a6671db259ad33c4e13
Your Name <tarang5757@gmail.com>
2025-06-26 16:00:23 -0400

    "Refactored App.jsx to change layout for register/login compared to Store pages (homepage store, products). Refactored cleaned up Footer/Navbar with more optimal approach.  added /login and /signup pages."

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,12 +1,14 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
 import "./index.css";
-import App from "./App";
 
+import { BrowserRouter } from "react-router-dom";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-    <React.StrictMode>
+createRoot(document.getElementById("root")).render(
+    <StrictMode>
+        <BrowserRouter>
             <App />
-    </React.StrictMode>
+        </BrowserRouter>
+    </StrictMode>
 );
