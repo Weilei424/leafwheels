349ab8d2857d4d917e07134d097cdad76182beb8
Mason Wang <mason123wp@gmail.com>
2025-06-26 03:54:18 -0400

    Add filterVehicles method to VehicleService for advanced vehicle filtering

--- leafwheels/src/main/java/com/yorku4413s25/leafwheels/services/VehicleService.java
+++ leafwheels/src/main/java/com/yorku4413s25/leafwheels/services/VehicleService.java
@@ -1,7 +1,15 @@
 package com.yorku4413s25.leafwheels.services;
 
+import com.yorku4413s25.leafwheels.constants.BodyType;
+import com.yorku4413s25.leafwheels.constants.Condition;
+import com.yorku4413s25.leafwheels.constants.Make;
+import com.yorku4413s25.leafwheels.constants.VehicleStatus;
+import com.yorku4413s25.leafwheels.domain.Vehicle;
 import com.yorku4413s25.leafwheels.web.models.VehicleDto;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
+import java.math.BigDecimal;
 import java.util.List;
 import java.util.UUID;
 
@@ -15,4 +23,24 @@ public interface VehicleService {
     void delete(UUID vehicleId);
 
     List<VehicleDto> getAllVehicles();
+
+    Page<Vehicle> filterVehicles(
+            Integer year,
+            Make make,
+            String model,
+            BodyType bodyType,
+            String exteriorColor,
+            Integer doors,
+            Integer seats,
+            Integer minMileage,
+            Integer maxMileage,
+            Integer minBatteryRange,
+            Integer maxBatteryRange,
+            BigDecimal minPrice,
+            BigDecimal maxPrice,
+            Boolean onDeal,
+            Condition condition,
+            VehicleStatus status,
+            Pageable pageable
+    );
 }
