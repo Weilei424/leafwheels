2e250dc6723b436e10315a6a306df451567410bb
Mason Wang <mason123wp@gmail.com>
2025-06-26 02:28:58 -0400

    Add queries to VehicleRepository for distinct vehicle models and exterior colors

--- leafwheels/src/main/java/com/yorku4413s25/leafwheels/repositories/VehicleRepository.java
+++ leafwheels/src/main/java/com/yorku4413s25/leafwheels/repositories/VehicleRepository.java
@@ -2,10 +2,19 @@ package com.yorku4413s25.leafwheels.repositories;
 
 import com.yorku4413s25.leafwheels.domain.Vehicle;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface VehicleRepository extends JpaRepository<Vehicle, UUID> {
+
+    @Query("SELECT DISTINCT v.model FROM Vehicle v WHERE v.model IS NOT NULL")
+    List<String> findDistinctModels();
+
+    @Query("SELECT DISTINCT v.exteriorColor FROM Vehicle v WHERE v.exteriorColor IS NOT NULL")
+    List<String> findDistinctExteriorColor();
+
 }
