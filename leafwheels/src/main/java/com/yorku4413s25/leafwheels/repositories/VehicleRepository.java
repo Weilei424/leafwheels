d628f44d4885f7bac08d08967e4ff709dc19959f
Mason Wang <mason123wp@gmail.com>
2025-06-27 17:33:12 -0400

    Add JpaSpecificationExecutor to VehicleRepository for enhanced querying capabilities

--- leafwheels/src/main/java/com/yorku4413s25/leafwheels/repositories/VehicleRepository.java
+++ leafwheels/src/main/java/com/yorku4413s25/leafwheels/repositories/VehicleRepository.java
@@ -2,6 +2,7 @@ package com.yorku4413s25.leafwheels.repositories;
 
 import com.yorku4413s25.leafwheels.domain.Vehicle;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
@@ -9,7 +10,7 @@ import java.util.List;
 import java.util.UUID;
 
 @Repository
-public interface VehicleRepository extends JpaRepository<Vehicle, UUID> {
+public interface VehicleRepository extends JpaRepository<Vehicle, UUID>, JpaSpecificationExecutor<Vehicle> {
 
     @Query("SELECT DISTINCT v.model FROM Vehicle v WHERE v.model IS NOT NULL")
     List<String> findDistinctModels();
