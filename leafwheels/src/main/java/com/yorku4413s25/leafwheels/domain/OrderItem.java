4c4bacde8fdfec69f363784dca96767bd50e3c4e
Mason Wang <mason123wp@gmail.com>
2025-06-29 02:12:24 -0400

    Add OrderItem entity with properties for order management

---
+++ leafwheels/src/main/java/com/yorku4413s25/leafwheels/domain/OrderItem.java
@@ -0,0 +1,43 @@
+package com.yorku4413s25.leafwheels.domain;
+
+import com.yorku4413s25.leafwheels.constants.OrderItemType;
+import jakarta.persistence.*;
+import lombok.*;
+import java.math.BigDecimal;
+import java.util.UUID;
+
+@Entity
+@Table(name = "order_items")
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class OrderItem extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "order_id", nullable = false)
+    private Order order;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private OrderItemType type;
+
+    // only if OrderItemType is VEHICLE
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "vehicle_id")
+    private Vehicle vehicle;
+
+    // only if OrderItemType is ACCESSORY
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "accessory_id")
+    private Accessory accessory;
+
+    @Column(nullable = false)
+    private BigDecimal unitPrice;
+
+    @Column(nullable = false)
+    private int quantity;
+}
