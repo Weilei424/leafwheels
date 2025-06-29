66dd982c45f97d5a4acb89cafb3ed62c01176247
Mason Wang <mason123wp@gmail.com>
2025-06-29 18:21:57 -0400

    Add mappers for Accessory, OrderItem, and User to facilitate DTO conversions

---
+++ leafwheels/src/main/java/com/yorku4413s25/leafwheels/web/mappers/OrderItemMapper.java
@@ -0,0 +1,13 @@
+package com.yorku4413s25.leafwheels.web.mappers;
+
+import com.yorku4413s25.leafwheels.domain.OrderItem;
+import com.yorku4413s25.leafwheels.web.models.OrderItemDto;
+import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
+
+@Mapper(componentModel = "spring")
+public interface OrderItemMapper {
+    OrderItemDto orderItemToOrderItemDto(OrderItem orderItem);
+    OrderItem orderItemDtoToOrderItem(OrderItemDto orderItemDto);
+    void orderItemDtoToOrderItemUpdate(OrderItemDto orderItemDto, @MappingTarget OrderItem existing);
+}
