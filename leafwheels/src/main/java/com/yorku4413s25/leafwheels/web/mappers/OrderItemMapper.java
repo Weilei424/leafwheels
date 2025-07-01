4fa446f57a6aa7f8b0041ebff823dd49dcec3a4b
Mason Wang <mason123wp@gmail.com>
2025-07-01 01:23:06 -0400

    Enhance OrderItemMapper to utilize VehicleMapper and AccessoryMapper for improved DTO conversions

--- leafwheels/src/main/java/com/yorku4413s25/leafwheels/web/mappers/OrderItemMapper.java
+++ leafwheels/src/main/java/com/yorku4413s25/leafwheels/web/mappers/OrderItemMapper.java
@@ -5,7 +5,7 @@ import com.yorku4413s25.leafwheels.web.models.OrderItemDto;
 import org.mapstruct.Mapper;
 import org.mapstruct.MappingTarget;
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring", uses = {VehicleMapper.class, AccessoryMapper.class})
 public interface OrderItemMapper {
     OrderItemDto orderItemToOrderItemDto(OrderItem orderItem);
     OrderItem orderItemDtoToOrderItem(OrderItemDto orderItemDto);
