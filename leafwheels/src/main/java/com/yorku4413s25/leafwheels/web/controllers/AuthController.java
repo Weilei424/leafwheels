3b11d8690c1c9ff25297694f680613321dbb5361
Mason Wang <mason123wp@gmail.com>
2025-07-02 05:27:36 -0400

    Refactor AuthController to use AuthServiceImpl and add UserService dependency (for dev/test)

--- leafwheels/src/main/java/com/yorku4413s25/leafwheels/web/controllers/AuthController.java
+++ leafwheels/src/main/java/com/yorku4413s25/leafwheels/web/controllers/AuthController.java
@@ -1,24 +1,28 @@
 package com.yorku4413s25.leafwheels.web.controllers;
-import com.yorku4413s25.leafwheels.services.AuthService;
+import com.yorku4413s25.leafwheels.services.AuthServiceImpl;
+import com.yorku4413s25.leafwheels.services.UserService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 
 @RestController
 @RequestMapping("/api/v1/auth")
+@RequiredArgsConstructor
 public class AuthController {
 
-    private AuthService authService;
+    private final AuthServiceImpl authServiceImpl;
+    private final UserService userService;
 
     @PostMapping("/register")
     public ResponseEntity<String> register(@RequestParam String email, @RequestParam String password) {
-        String response = authService.register(email, password);
+        String response = authServiceImpl.register(email, password);
         return ResponseEntity.ok(response);
     }
 
     @PostMapping("/login")
     public ResponseEntity<String> login(@RequestParam String email, @RequestParam String password) {
-        String response = authService.login(email, password);
+        String response = authServiceImpl.login(email, password);
         return ResponseEntity.ok(response);
     }
 }
