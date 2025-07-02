f9740995e76f7e2f8209fb7778acb05e8e3ae1c8
Mason Wang <mason123wp@gmail.com>
2025-07-01 23:47:43 -0400

    Add method to retrieve orders by user ID in OrderRepository

--- leafwheels/src/main/java/com/yorku4413s25/leafwheels/repositories/OrderRepository.java
+++ leafwheels/src/main/java/com/yorku4413s25/leafwheels/repositories/OrderRepository.java
@@ -3,7 +3,9 @@ package com.yorku4413s25.leafwheels.repositories;
 import com.yorku4413s25.leafwheels.domain.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface OrderRepository extends JpaRepository<Order, UUID> {
+    List<Order> findAllByUserId(UUID userId);
 }
