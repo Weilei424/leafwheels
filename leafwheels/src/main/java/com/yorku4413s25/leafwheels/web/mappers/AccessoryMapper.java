5149919a1918807f48e117cc93ed2504cc03d549
Mason Wang <mason123wp@gmail.com>
2025-07-02 04:53:10 -0400

    Fix typo

--- leafwheels/src/main/java/com/yorku4413s25/leafwheels/web/mappers/AccessoryMapper.java
+++ leafwheels/src/main/java/com/yorku4413s25/leafwheels/web/mappers/AccessoryMapper.java
@@ -1,12 +1,13 @@
 package com.yorku4413s25.leafwheels.web.mappers;
 
+import com.yorku4413s25.leafwheels.domain.Accessory;
 import com.yorku4413s25.leafwheels.web.models.AccessoryDto;
 import org.mapstruct.Mapper;
 import org.mapstruct.MappingTarget;
 
 @Mapper(componentModel = "spring")
 public interface AccessoryMapper {
-    AccessoryDto accessoryDtoToAccessoryDto(AccessoryDto accessoryDto);
-    AccessoryDto accessoryToAccessoryDto(AccessoryDto accessory);
+    Accessory accessoryDtoToAccessory(AccessoryDto accessoryDto);
+    AccessoryDto accessoryToAccessoryDto(Accessory accessory);
     void accessoryDtoToAccessoryUpdate(AccessoryDto accessoryDto, @MappingTarget AccessoryDto existing);
 }
