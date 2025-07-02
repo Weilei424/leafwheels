3f1a3e450bfc6d3a005efe2a84f429b569a0368d
Mason Wang <mason123wp@gmail.com>
2025-07-02 04:58:55 -0400

    Implement AccessoryController with endpoints for managing accessories

---
+++ leafwheels/src/main/java/com/yorku4413s25/leafwheels/web/controllers/AccessoryController.java
@@ -0,0 +1,32 @@
+package com.yorku4413s25.leafwheels.web.controllers;
+
+import com.yorku4413s25.leafwheels.services.AccessoryService;
+import com.yorku4413s25.leafwheels.web.models.AccessoryDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/v1/accessories")
+@RequiredArgsConstructor
+public class AccessoryController {
+
+    private final AccessoryService accessoryService;
+
+    @GetMapping("/all")
+    public List<AccessoryDto> getAllAccessories() {
+        return accessoryService.getAllAccessories();
+    }
+
+    @GetMapping("/{id}")
+    public AccessoryDto getAccessory(@PathVariable UUID id) {
+        return accessoryService.getAccessoryById(id);
+    }
+
+    @PostMapping
+    public AccessoryDto createAccessory(@RequestBody AccessoryDto dto) {
+        return accessoryService.createAccessory(dto);
+    }
+}
