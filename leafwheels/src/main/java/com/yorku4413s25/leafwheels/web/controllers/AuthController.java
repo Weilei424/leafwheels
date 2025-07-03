822b86e9ae8aaf7be72a7ea608ad75dae35d4a99
Mason Wang <mason123wp@gmail.com>
2025-07-02 23:01:39 -0400

    Add development endpoints for user registration and login in AuthController

--- leafwheels/src/main/java/com/yorku4413s25/leafwheels/web/controllers/AuthController.java
+++ leafwheels/src/main/java/com/yorku4413s25/leafwheels/web/controllers/AuthController.java
@@ -1,7 +1,9 @@
 package com.yorku4413s25.leafwheels.web.controllers;
 import com.yorku4413s25.leafwheels.services.AuthServiceImpl;
 import com.yorku4413s25.leafwheels.services.UserService;
+import com.yorku4413s25.leafwheels.web.models.UserDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -24,6 +26,22 @@ public class AuthController {
     public ResponseEntity<String> login(@RequestParam String email, @RequestParam String password) {
         String response = authServiceImpl.login(email, password);
         return ResponseEntity.ok(response);
+    }
+
+    @PostMapping("/dev/register")
+    public ResponseEntity<UserDto> devRegister(@RequestBody UserDto userDto) {
+        return new ResponseEntity<>(userService.signup(
+                userDto.getFirstName(),
+                userDto.getLastName(),
+                userDto.getEmail(),
+                userDto.getPassword()), HttpStatus.OK);
+    }
+
+    @PostMapping("/dev/login")
+    public ResponseEntity<UserDto> devLogin(@RequestBody UserDto userDto) {
+        return new ResponseEntity<>(userService.login(
+                userDto.getEmail(),
+                userDto.getPassword()), HttpStatus.OK);
     }
 }
 
