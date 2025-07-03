4fd5c8dea6d6bfda4a769ba4513ab17f451d6343
Mason Wang <mason123wp@gmail.com>
2025-07-03 03:35:04 -0400

    Refactor CartController to return ResponseEntity for cart operations

--- leafwheels/src/main/java/com/yorku4413s25/leafwheels/web/controllers/CartController.java
+++ leafwheels/src/main/java/com/yorku4413s25/leafwheels/web/controllers/CartController.java
@@ -4,6 +4,8 @@ import com.yorku4413s25.leafwheels.services.CartService;
 import com.yorku4413s25.leafwheels.web.models.CartDto;
 import com.yorku4413s25.leafwheels.web.models.CreateCartItemDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
@@ -16,22 +18,23 @@ public class CartController {
     private final CartService cartService;
 
     @GetMapping("/{userId}")
-    public CartDto getCart(@PathVariable UUID userId) {
-        return cartService.getCartByUserId(userId);
+    public ResponseEntity<CartDto> getCart(@PathVariable UUID userId) {
+        return new ResponseEntity<>(cartService.getCartByUserId(userId), HttpStatus.OK);
     }
 
     @PostMapping("/{userId}/items")
-    public CartDto addItem(@PathVariable UUID userId, @RequestBody CreateCartItemDto dto) {
-        return cartService.addItemToCart(userId, dto);
+    public ResponseEntity<CartDto> addItem(@PathVariable UUID userId, @RequestBody CreateCartItemDto dto) {
+        return new ResponseEntity<>(cartService.addItemToCart(userId, dto), HttpStatus.OK);
     }
 
     @DeleteMapping("/{userId}/items/{itemId}")
-    public CartDto removeItem(@PathVariable UUID userId, @PathVariable UUID itemId) {
-        return cartService.removeItemFromCart(userId, itemId);
+    public ResponseEntity<CartDto> removeItem(@PathVariable UUID userId, @PathVariable UUID itemId) {
+        return new ResponseEntity<>(cartService.removeItemFromCart(userId, itemId), HttpStatus.OK);
     }
 
     @DeleteMapping("/{userId}")
-    public void clearCart(@PathVariable UUID userId) {
+    public ResponseEntity<Void> clearCart(@PathVariable UUID userId) {
         cartService.clearCart(userId);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 }
