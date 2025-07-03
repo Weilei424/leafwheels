81fb2d97ff783c167a12133023f76da138e88794
Mason Wang <mason123wp@gmail.com>
2025-07-03 03:35:20 -0400

    Refactor AccessoryController to return ResponseEntity for accessory operations

--- leafwheels/src/main/java/com/yorku4413s25/leafwheels/web/controllers/AccessoryController.java
+++ leafwheels/src/main/java/com/yorku4413s25/leafwheels/web/controllers/AccessoryController.java
@@ -3,6 +3,8 @@ package com.yorku4413s25.leafwheels.web.controllers;
 import com.yorku4413s25.leafwheels.services.AccessoryService;
 import com.yorku4413s25.leafwheels.web.models.AccessoryDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -16,18 +18,18 @@ public class AccessoryController {
     private final AccessoryService accessoryService;
 
     @GetMapping("/all")
-    public List<AccessoryDto> getAllAccessories() {
-        return accessoryService.getAllAccessories();
+    public ResponseEntity<List<AccessoryDto>> getAllAccessories() {
+        return new ResponseEntity<>(accessoryService.getAllAccessories(), HttpStatus.OK);
     }
 
     @GetMapping("/{id}")
-    public AccessoryDto getAccessory(@PathVariable UUID id) {
-        return accessoryService.getAccessoryById(id);
+    public ResponseEntity<AccessoryDto> getAccessory(@PathVariable UUID id) {
+        return new ResponseEntity<>(accessoryService.getAccessoryById(id), HttpStatus.OK);
     }
 
     @PostMapping
-    public AccessoryDto createAccessory(@RequestBody AccessoryDto dto) {
-        return accessoryService.createAccessory(dto);
+    public ResponseEntity<AccessoryDto> createAccessory(@RequestBody AccessoryDto dto) {
+        return new ResponseEntity<>(accessoryService.createAccessory(dto), HttpStatus.CREATED);
     }
 
     //TODO: Implement update and delete methods
