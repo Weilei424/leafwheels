75d63caebb40043aeff5dc422d3de86943e1b219
Your Name <tarang5757@gmail.com>
2025-07-08 16:50:29 -0400

    "Cleaned up StorePage by eliminating clutter code, breaking complex code apart into reuseable components, moving dummy data /data

---
+++ frontend/src/data/products.js
@@ -0,0 +1,57 @@
+const products = [
+{
+    _id: "1",
+        name: "Tesla Model 3",
+    price: 39990.0,
+    image:
+    "https://digitalassets.tesla.com/tesla-contents/image/upload/f_auto,q_auto/Model-3-Main-Hero-Desktop-LHD.jpg",
+        category: "Electric Cars",
+    createdAt: "2024-06-01",
+},
+{
+    _id: "2",
+        name: "Nissan Leaf",
+    price: 27400.0,
+    image:
+    "https://hips.hearstapps.com/hmg-prod/images/2025-nissan-leaf-121-6690462ff1899.jpg?auto=format&fit=crop&w=600&q=80",
+        category: "Electric Cars",
+    createdAt: "2024-05-15",
+},
+{
+    _id: "3",
+        name: "EV Charging Cable",
+    price: 149.99,
+    image: "https://m.media-amazon.com/images/I/61usxWzJdkL.jpg",
+    category: "Chargers",
+    createdAt: "2024-04-10",
+},
+{
+    _id: "4",
+        name: "Wall Charger Station",
+    price: 349.99,
+    image:
+    "https://evocharge.com/wp-content/uploads/2021/02/GettyImages-1249775796.jpg",
+        category: "Chargers",
+    createdAt: "2024-06-10",
+},
+{
+    _id: "5",
+        name: "Portable EV Charger",
+    price: 599.99,
+    image:
+    "https://www.shutterstock.com/image-photo/portable-ev-charger-electric-vehicle-600nw-2314986427.jpg",
+        category: "Chargers",
+    createdAt: "2024-03-22",
+},
+{
+    _id: "6",
+        name: "Car Cover",
+    price: 149.99,
+    image:
+    "https://images.weathertech.com/7456a866-b1d2-4634-82b0-b1a8013eb661/bmw_m3_car_cover_darkblue_Original%20file.jpg?width=800&height=800&fit=bounds",
+        category: "Accessories",
+    createdAt: "2024-01-05",
+},
+    ];
+
+export default products;
