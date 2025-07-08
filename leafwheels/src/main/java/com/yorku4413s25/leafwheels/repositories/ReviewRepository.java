c0d471918f8cb40db9a6c609dca4473816c3609e
manali bisht <manali.bisht17@gmail.com>
2025-07-08 17:53:36 -0400

    Review

--- leafwheels/src/main/java/com/yorku4413s25/leafwheels/repositories/ReviewRepository.java
+++ leafwheels/src/main/java/com/yorku4413s25/leafwheels/repositories/ReviewRepository.java
@@ -2,11 +2,12 @@ package com.yorku4413s25.leafwheels.repositories;
 
 import com.yorku4413s25.leafwheels.domain.Review;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.UUID;
 
-
+@Repository
 public interface ReviewRepository extends JpaRepository<Review, UUID> {
     List<Review> findByUserId(UUID userId);
     List<Review> findByVehicleId(UUID vehicleId);
--- leafwheels/src/main/java/com/yorku4413s25/leafwheels/web/mappers/ReviewMapper.java
+++ leafwheels/src/main/java/com/yorku4413s25/leafwheels/web/mappers/ReviewMapper.java
@@ -5,6 +5,7 @@ import com.yorku4413s25.leafwheels.web.models.ReviewDto;
 import org.mapstruct.Mapper;
 import org.mapstruct.MappingTarget;
 
+@Mapper(componentModel = "spring")
 public interface ReviewMapper {
     ReviewDto reviewToReviewDto(Review review);
     Review reviewDtoToReview(ReviewDto dto);
