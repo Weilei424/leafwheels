9618d01698b120213ecf6573a92ea30a7d819703
Your Name <tarang5757@gmail.com>
2025-07-08 16:51:55 -0400

    "removed CDN bc it won't work on production. Added tailwind packages by directly installing it into the system. Added fonts for good ui."

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,12 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter', 'ui-sans-serif', 'system-ui'],
+      },
+    },
+  },
   server: {
     proxy: {
       '/api': 'http://localhost:8080/',
-    }
-  }
+    },
+  },
 })
