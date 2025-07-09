bf5606c45872d6d69964ec0772cbd90a4d081d27
Mason Wang <mason123wp@gmail.com>
2025-07-09 14:03:30 -0400

    Uses VehicleHistoryMapper.class to map properly

--- leafwheels/src/main/java/com/yorku4413s25/leafwheels/web/mappers/VehicleMapper.java
+++ leafwheels/src/main/java/com/yorku4413s25/leafwheels/web/mappers/VehicleMapper.java
@@ -1,11 +1,12 @@
 package com.yorku4413s25.leafwheels.web.mappers;
 
 import com.yorku4413s25.leafwheels.domain.Vehicle;
+import com.yorku4413s25.leafwheels.domain.VehicleHistory;
 import com.yorku4413s25.leafwheels.web.models.VehicleDto;
 import org.mapstruct.Mapper;
 import org.mapstruct.MappingTarget;
 
-@Mapper(uses = DateMapper.class, componentModel = "spring")
+@Mapper(uses = {DateMapper.class, VehicleHistoryMapper.class}, componentModel = "spring")
 public interface VehicleMapper {
     VehicleDto vehicleToVehicleDto(Vehicle vehicle);
     Vehicle vehicleDtoToVehicle(VehicleDto vehicleDto);
