8a8af26e735231de2f57d79673475933e0e9222c
Mason Wang <mason123wp@gmail.com>
2025-07-17 19:17:03 -0400

    Add ecr config, need to check if really need ecr

---
+++ terraform/modules/ecr/variables.tf
@@ -0,0 +1,15 @@
+variable "name_prefix" {
+  description = "Name prefix for resources"
+  type        = string
+}
+
+variable "environment" {
+  description = "Environment name"
+  type        = string
+}
+
+variable "tags" {
+  description = "Tags to apply to resources"
+  type        = map(string)
+  default     = {}
+}
