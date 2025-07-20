8576f21e73c44e671320f098767004d67c313fd2
Mason Wang <mason123wp@gmail.com>
2025-07-20 04:32:53 -0400

    Update redis template for jwt

--- leafwheels/src/main/java/com/yorku4413s25/leafwheels/config/RedisSessionConfig.java
+++ leafwheels/src/main/java/com/yorku4413s25/leafwheels/config/RedisSessionConfig.java
@@ -1,9 +1,28 @@
 package com.yorku4413s25.leafwheels.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 @Configuration
 @EnableRedisHttpSession(maxInactiveIntervalInSeconds = 3600)
 public class RedisSessionConfig {
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(connectionFactory);
+
+        StringRedisSerializer stringSerializer = new StringRedisSerializer();
+        template.setKeySerializer(stringSerializer);
+        template.setValueSerializer(stringSerializer);
+        template.setHashKeySerializer(stringSerializer);
+        template.setHashValueSerializer(stringSerializer);
+        
+        template.afterPropertiesSet();
+        return template;
+    }
 }
