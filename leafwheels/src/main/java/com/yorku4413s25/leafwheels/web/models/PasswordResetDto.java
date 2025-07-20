c27bb22c7a1212905496e95686b145be1f3c8f5c
Mason Wang <mason123wp@gmail.com>
2025-07-20 01:43:51 -0400

    Add DTOs

---
+++ leafwheels/src/main/java/com/yorku4413s25/leafwheels/web/models/PasswordResetDto.java
@@ -0,0 +1,25 @@
+package com.yorku4413s25.leafwheels.web.models;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class PasswordResetDto {
+
+    @NotBlank(message = "Token is required")
+    private String token;
+
+    @NotBlank(message = "New password is required")
+    @Size(min = 8, max = 100, message = "Password must be between 8 and 100 characters")
+    private String newPassword;
+
+    @NotBlank(message = "Confirm password is required")
+    private String confirmPassword;
+}
