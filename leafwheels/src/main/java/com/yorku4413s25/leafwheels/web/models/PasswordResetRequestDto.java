47898f3b22ccf86c3ad1c35d40be6c96eff280cc
Mason Wang <mason123wp@gmail.com>
2025-07-20 01:43:51 -0400

    Add DTOs

---
+++ leafwheels/src/main/java/com/yorku4413s25/leafwheels/web/models/PasswordResetRequestDto.java
@@ -0,0 +1,19 @@
+package com.yorku4413s25.leafwheels.web.models;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class PasswordResetRequestDto {
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Email should be valid")
+    private String email;
+}
