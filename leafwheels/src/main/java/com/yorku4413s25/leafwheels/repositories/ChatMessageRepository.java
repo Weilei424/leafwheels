58d126e86f0ec96127dd254a24533de614a775c6
Mason Wang <mason123wp@gmail.com>
2025-07-29 03:25:53 -0400

    Remove unused methods

--- leafwheels/src/main/java/com/yorku4413s25/leafwheels/repositories/ChatMessageRepository.java
+++ leafwheels/src/main/java/com/yorku4413s25/leafwheels/repositories/ChatMessageRepository.java
@@ -19,14 +19,8 @@ public interface ChatMessageRepository extends JpaRepository<ChatMessage, Long> 
     
     Page<ChatMessage> findByChatSessionOrderByCreatedAtDesc(ChatSession chatSession, Pageable pageable);
     
-    @Query("SELECT cm FROM ChatMessage cm WHERE cm.chatSession = :session AND cm.createdAt > :since ORDER BY cm.createdAt ASC")
-    List<ChatMessage> findRecentMessages(@Param("session") ChatSession session, @Param("since") Instant since);
-    
     @Query("SELECT COUNT(cm) FROM ChatMessage cm WHERE cm.chatSession.user.id = :userId AND cm.createdAt > :since AND cm.isFromUser = true")
     long countUserMessagesSince(@Param("userId") Long userId, @Param("since") Instant since);
-    
-    @Query("SELECT cm FROM ChatMessage cm WHERE cm.chatSession = :session AND cm.intent IS NOT NULL ORDER BY cm.createdAt DESC")
-    List<ChatMessage> findIntentMessages(@Param("session") ChatSession session, Pageable pageable);
     
     void deleteByChatSession(ChatSession chatSession);
 }
